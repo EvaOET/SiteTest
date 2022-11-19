b29c2e890188f317637dd99b8d0a90e98cb105a0
Eva OET <eva@oetonline.net.au>
2022-11-19 15:14:21 +1000

    update latest

---
+++ app.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { render } from "react-dom";
+
+render(<div>Testing</div>, document.getElementById("mainSite"));
---
+++ build/index.js
@@ -0,0 +1,12 @@
+var $iggwW$reactjsxruntime = require("react/jsx-runtime");
+require("react");
+var $iggwW$reactdom = require("react-dom");
+
+
+
+
+(0, $iggwW$reactdom.render)(/*#__PURE__*/ (0, $iggwW$reactjsxruntime.jsx)("div", {
+    children: "Testing"
+}), document.getElementById("mainSite"));
+
+
