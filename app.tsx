aac99253b388e206acbc3c3727224f61d139cab8
Eva OET <eva@oetonline.net.au>
2022-11-19 15:59:49 +1000

    update latest

--- app.tsx
+++ app.tsx
@@ -2,22 +2,25 @@ import { Button, Fab } from "@mui/material";
 import React from "react";
 import { render } from "react-dom";
 import ContentContainer from "./Components/ContentContainer";
-import ExampleItem from "./Components/ExampleItem";
 import NavigationIcon from '@mui/icons-material/Navigation';
+import { useState } from "react";
+import CourseMenu from "./Components/CourseMenu";
 
-let mainPage = (
-    <ContentContainer>
-        <ExampleItem Name="Item 1" />
-        <ExampleItem Name="Item 2" />
-        <ExampleItem Name="Item 3" />
-        <div style={{width: 500}}>
-        <Fab variant="extended">
-            <NavigationIcon sx={{ mr: 1 }} />
-            Navigate
-        </Fab>
-        <Button variant="contained">Example Button</Button>
-        </div>
-    </ContentContainer>
-)
 
-render(mainPage, document.getElementById("mainPage"));
+let MainPage = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    const toggleState = () => setShowMenu((val) => !val);
+    return (
+        <ContentContainer>
+            <div style={{}}>
+                <Fab variant="extended" onClick={toggleState}>
+                    <NavigationIcon sx={{ mr: 1 }} />
+                    {!showMenu ? "Show Menu" : "Hide Menu"}
+                </Fab>
+                {showMenu && <CourseMenu />}
+            </div>
+        </ContentContainer>
+    )
+};
+
+render(<MainPage />, document.getElementById("mainPage"));
