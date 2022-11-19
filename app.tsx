89ffc752d31b14d5e25995a3daafb5b35404375e
Eva OET <eva@oetonline.net.au>
2022-11-19 15:44:13 +1000

    add buttons

--- app.tsx
+++ app.tsx
@@ -1,14 +1,22 @@
+import { Button, Fab } from "@mui/material";
 import React from "react";
 import { render } from "react-dom";
 import ContentContainer from "./Components/ContentContainer";
 import ExampleItem from "./Components/ExampleItem";
-
+import NavigationIcon from '@mui/icons-material/Navigation';
 
 let mainPage = (
     <ContentContainer>
-        <ExampleItem Name="Item 1"/>
-        <ExampleItem Name="Item 2"/>
-        <ExampleItem Name="Item 3"/>
+        <ExampleItem Name="Item 1" />
+        <ExampleItem Name="Item 2" />
+        <ExampleItem Name="Item 3" />
+        <div style={{width: 500}}>
+        <Fab variant="extended">
+            <NavigationIcon sx={{ mr: 1 }} />
+            Navigate
+        </Fab>
+        <Button variant="contained">Example Button</Button>
+        </div>
     </ContentContainer>
 )
 
